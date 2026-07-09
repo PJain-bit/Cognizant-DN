82ca81c98513f66665d2ebb207abf60fd68178b6
Purvi Jain <jainpurvi589@gmail.com>
2026-07-09 15:17:05 +0530

    After sucessfull creation of Account and Loan Microservices, Week 4 hands-on completed

---
+++ JavaFSE/Deepskilling/account-service/src/main/java/com/cognizant/account_service/controller/AccountController.java
@@ -0,0 +1,22 @@
+package com.cognizant.account_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.account_service.model.Account;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/accounts/{number}")
+    public Account getAccount(@PathVariable String number) {
+
+        return new Account(
+                number,
+                "savings",
+                234343
+        );
+    }
+
+}
